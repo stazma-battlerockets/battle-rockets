a3b422c0cd430d5279ff0a22a9b91c51d747d138
Maxim Belomestnov <maximbel@Maxims-MacBook-Air.local>
2021-10-08 17:02:50 -0400

    Added partials and connected API using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.scss";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  axios({
+    url: "https://api.spacexdata.com/v4/rockets",
+  }).then((results) => {
+    console.log(results);
+  });
+
+  return (
+    <div className="wrapper">
+      <h1>Battle Rockets</h1>
+    </div>
+  );
 }
 
 export default App;
