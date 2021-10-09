20d3d99726454d752314f331062e063d235ff961
Maxim Belomestnov <maximbel@Maxims-MacBook-Air.local>
2021-10-09 18:04:56 -0400

    Pulled latest commit from github

--- src/components/BattleRocketGrid.js
+++ src/components/BattleRocketGrid.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const BattleRocketGrid = () => {
-
   const [ship, setShip] = useState({});
 
   const gridArray = [];
@@ -10,29 +9,43 @@ const BattleRocketGrid = () => {
     gridArray.push(i);
   }
 
-
   // An array of objects -> ship name and X and Y coords
   const allShips = [
     {
       name: "destroyer",
-      coords: [[0, 1], [0, 10]]
+      coords: [
+        [0, 1],
+        [0, 10],
+      ],
     },
     {
       name: "submarine",
-      coords: [[0, 1, 2], [0, 10, 20]]
+      coords: [
+        [0, 1, 2],
+        [0, 10, 20],
+      ],
     },
     {
       name: "cruiser",
-      coords: [[0, 1, 2], [0, 10, 20]]
+      coords: [
+        [0, 1, 2],
+        [0, 10, 20],
+      ],
     },
     {
       name: "battleship",
-      coords: [[0, 1, 2, 3], [0, 10, 20, 30]]
+      coords: [
+        [0, 1, 2, 3],
+        [0, 10, 20, 30],
+      ],
     },
     {
       name: "carrier",
-      coords: [[0, 1, 2, 3, 4], [0, 10, 20, 30, 40]]
-    }
+      coords: [
+        [0, 1, 2, 3, 4],
+        [0, 10, 20, 30, 40],
+      ],
+    },
   ];
 
   // console.log(ships);
@@ -50,30 +63,28 @@ const BattleRocketGrid = () => {
     newShip.coords[1] = newShip.coords[1].map((coord) => {
       return coord + index;
     });
-    
+
     setShip(newShip);
-
-
   };
-
 
   return (
     <section className="grid">
       <div className="gridContent">
-        {
-          gridArray.map((gridIndex) => {
-            return (
-              <div
-                // The 0 is hard coded for horizontal coords
-                className={`gridSquare ${ship.coords[0].includes(gridIndex) ? 'blue' : null}`}
-                key={`square${gridIndex}`}
-                // The 3 is hard coded for a battleship
-                onClick={() => handleClick(3, gridIndex)}></div>
-            )
-          })
-        }
+        {gridArray.map((gridIndex) => {
+          return (
+            <div
+              // The 0 is hard coded for horizontal coords
+              className={`gridSquare ${
+                ship.coords[0].includes(gridIndex) ? "blue" : null
+              }`}
+              key={`square${gridIndex}`}
+              // The 3 is hard coded for a battleship
+              onClick={() => handleClick(3, gridIndex)}
+            ></div>
+          );
+        })}
       </div>
-    </section >
+    </section>
   );
 };
 
