786f0c428a6023e5c55cbad94e41b6045360d83c
Maxim Belomestnov <maximbel@Maxims-MacBook-Air.local>
2021-10-11 18:36:54 -0400

    Reviewed comments

--- src/components/BattleRocketGrid.js
+++ src/components/BattleRocketGrid.js
@@ -1,10 +1,11 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 const BattleRocketGrid = () => {
-  // State to hold ship object
+  // State to hold our ship object
   const [ship, setShip] = useState({
     name: "default",
     coords: [],
+    placed: null,
   });
 
   // State to hold ship direction
@@ -13,13 +14,13 @@ const BattleRocketGrid = () => {
   // State to toggle the coordinate array index
   const [coordToggle, setCoordToggle] = useState(0);
 
-  // State to choose ship based on user click
+  // State to choose ship based on user click - default set to battlship
   const [shipType, setShipType] = useState(3);
 
   // State to display ships on the grid
   const [placedShips, setPlacedShips] = useState([]);
 
-  //
+  // State to remove onMouseEnter event when there is a click
   const [clicked, setClicked] = useState(false);
 
   // Initializing grid array
@@ -29,7 +30,7 @@ const BattleRocketGrid = () => {
     gridArray.push(i);
   }
 
-  // An array of objects -> ship name and X and Y coords
+  // An array of objects -> ship name, X and Y coords and if ship has been placed
   const allShips = [
     {
       name: "destroyer",
@@ -78,7 +79,7 @@ const BattleRocketGrid = () => {
     setCoordToggle(horizontal ? 0 : 1);
   }, [horizontal]);
 
-  // Used to toggle the orientation of the ship placement
+  // Used to toggle the orientation of the ship placement by right clicking on the grid
   const rotateShip = (e) => {
     if (e.button === 2) {
       setHorizontal(!horizontal);
@@ -90,7 +91,7 @@ const BattleRocketGrid = () => {
     // Our ship object
     const newShip = allShips[shipType];
 
-    // Placed ship coords array
+    // Placed ship coords array - either X or Y depending on coordToggle state
     const newCoords = newShip.coords[coordToggle].map((coord) => {
       return coord + index;
     });
@@ -113,7 +114,12 @@ const BattleRocketGrid = () => {
     }
   };
 
+  const addShipsToPage = () => {
+    setClicked(true);
+  };
+
   return (
+    // setShip type value hardcoded for each ship div so that shipType is toggled depending on which div is clicked
     <section className="gameBoard">
       <div className="shipSelection">
         <div className="destroyerSelect" onClick={() => setShipType(0)}></div>
@@ -122,7 +128,7 @@ const BattleRocketGrid = () => {
         <div className="battleshipSelect" onClick={() => setShipType(3)}></div>
         <div className="carrierSelect" onClick={() => setShipType(4)}></div>
 
-        <p>Right click to rotate.</p>
+        <p>Right click on grid to rotate ship.</p>
       </div>
       <div
         className="gridContent"
@@ -137,7 +143,7 @@ const BattleRocketGrid = () => {
               }`}
               key={`square${gridIndex}`}
               onMouseEnter={clicked ? null : () => handleMouseEnter(gridIndex)}
-              onClick={() => setClicked(!clicked)}
+              onClick={addShipsToPage}
             ></div>
           );
         })}
