1cfa8bcadbebbf3d617c8f23a16c1994b0df3877
maximbelo <mbelomestnov@hotmail.ca>
2023-05-13 11:00:23 -0400

    Removed pseudo code and getRockets feature from the main branch until it is ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import Header from "./components/Header";
 import PlayerSelection from "./components/PlayerSelection";
 import BattleRocketGrid from "./components/BattleRocketGrid";
-// import GetRockets from "./components/GetRockets";
 import Footer from "./components/Footer";
-
 import realtime from "./components/firebase.js";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -72,35 +70,19 @@ function App() {
             </div>
           </Route>
 
-          {/* <Route path="/player1/selection">
-            <GetRockets player={1} />
-          </Route> */}
-
           <Route path="/player1/game">
             <Header />
 
-            <BattleRocketGrid
-              setup={true}
-              player={1}
-              readyToPlay={readyToPlay}
-            />
+            <BattleRocketGrid setup={true} player={1} readyToPlay={readyToPlay} />
 
             {playerOneReady && playerTwoReady ? (
               <BattleRocketGrid setup={false} player={1} />
             ) : null}
           </Route>
 
-          {/* <Route path="/player2/selection">
-            <GetRockets player={2} />
-          </Route> */}
-
           <Route path="/player2/game">
             <Header />
-            <BattleRocketGrid
-              setup={true}
-              player={2}
-              readyToPlay={readyToPlay}
-            />
+            <BattleRocketGrid setup={true} player={2} readyToPlay={readyToPlay} />
 
             {playerOneReady && playerTwoReady ? (
               <BattleRocketGrid setup={false} player={2} />
@@ -114,40 +96,3 @@ function App() {
 }
 
 export default App;
-
-// // PSEUDOCODE //
-// Header component:
-//  - Welcome page (animated)
-//  - Create state for player 1 and player 2 - if one is selected - that state is set to true
-//    - If both states are set to true, the game can begin - save state in Firebase?
-
-//  // GAME SETUP //
-//  - after they choose player option, they will get to choose their ships/rockets (5 in total)
-//    - some stats of the rockets or ships are displayed
-//    - when they click on the rocket/ship, information about the rocket/ship is displayed
-//  - once all chosen, game boards will be displayed
-//    - 10 x 10 grid
-//    - a button to click when they're ready
-//  - players can place their ships on the board and rotate (NO CHEATING!)
-//    - state to store players board (ships' positions and orientations)
-//    - state to store the readiness of both players
-
-//  // GAME PLAY //
-//  - if both players are ready, load the play screen (both screens appear)
-//    - do sth to determine the player who goes first (or randomize)
-//    - store in state and switch state to determines which player is playing
-//  - player will click the opponent's grid, if the chosen square is in one the arrays stored in state, then we display it as a hit
-//    if not, sth else happens
-//  - create a counter for how many ships have been sunk by that player (MORE STATE)
-//    - after you blow up a rocket, elon musk picture appear on screen
-//    - if the counter hits 5, that player wins
-//  - reset button
-//    - when player clicks on the reset button, the screen goes back to the ships placement setup with option to choose another ships
-
-// // FUTURE QUESTION //
-// - what happens if one player leaves?
-
-// TODO
-// Control who's turn - firebase
-// Notify player if hit, miss, sunk and WIN/LOSE
-// Roster of each player
